be4a9f06b0708b50c21d724ba6c01a7e842ce944
Adam <gololobovadam@gmail.com>
2026-02-28 16:38:11 +0300

    mono fix 1

--- fastapi/alembic/versions/0013_bouquet_allow_flower_quantity.py
+++ fastapi/alembic/versions/0013_bouquet_allow_flower_quantity.py
@@ -1,6 +1,6 @@
 """add bouquet allow flower quantity flag
 
-Revision ID: 0013_bouquet_allow_flower_quantity
+Revision ID: 0013_bouquet_allow_flower_qty
 Revises: 0012_bouquet_type_and_style_csv
 Create Date: 2026-02-28 00:00:00.000000
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0013_bouquet_allow_flower_quantity"
+revision = "0013_bouquet_allow_flower_qty"
 down_revision = "0012_bouquet_type_and_style_csv"
 branch_labels = None
 depends_on = None
